6ed01b37c150141bc5817d45c1cd855b4ece3254
atsuji <ttakinai@yahoo.co.jp>
2025-11-15 23:47:33 +0900

    修正

--- Ruby/answers/1-6.rb
+++ Ruby/answers/1-6.rb
@@ -1,9 +1,9 @@
 def find_user_by_id(users, id)
-  users.find { |user| user.id == id}
+  users.find { |user| user[:id] == id}
 end
 
 def filter_adults(users)
-  users.select { |user| user.age >= 20 }
+  users.select { |user| user[:age] >= 20 }
 end
 
 def has_admin?(users)
